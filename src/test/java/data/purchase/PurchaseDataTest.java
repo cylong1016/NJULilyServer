763d13b5f8e0da72002804eca4f3cbabbb2c7c4b
cylong1016 <cyl19941016@sina.com>
2014-12-19 00:15:38 +0800

    进货单的数据可以读取

---
+++ src/test/java/data/purchase/PurchaseDataTest.java
@@ -0,0 +1,51 @@
+package data.purchase;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import po.PurchasePO;
+import data.purchasedata.PurchaseData;
+
+
+/**
+ * 
+ * @author cylong
+ * @version 2014年12月19日  上午12:12:45
+ */
+public class PurchaseDataTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 * @author cylong
+	 * @version 2014年12月19日  上午12:12:45
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 * @author cylong
+	 * @version 2014年12月19日  上午12:12:45
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link data.CommonData#show()}.
+	 * @throws RemoteException 
+	 */
+	@Test
+	public void testShow() throws RemoteException {
+		ArrayList<PurchasePO> pos = new PurchaseData().show();
+		for(PurchasePO po : pos) {
+			System.out.println(po.getID());
+		}
+	}
+
+}
