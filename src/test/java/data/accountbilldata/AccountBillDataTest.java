ff83c501a379cdeba3b74a66cdbc5961f9679fcb
cylong1016 <cyl19941016@sina.com>
2014-12-19 00:35:25 +0800

    AccountBillData的show测试；保存的时候忘记单据类型和ID了

--- src/test/java/data/accountbilldata/AccountBillDataTest.java
+++ src/test/java/data/accountbilldata/AccountBillDataTest.java
@@ -1,19 +1,21 @@
 package data.accountbilldata;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
-
+import po.AccountBillPO;
 
 /**
  * 测试get收款单和付款单的ID
  * @author cylong
- * @version 2014年12月17日  下午11:02:46
+ * @version 2014年12月17日 下午11:02:46
  */
 public class AccountBillDataTest {
 
 	AccountBillData accountBill;
+
 	/**
 	 * @see junit.framework.TestCase#setUp()
 	 */
@@ -28,22 +30,37 @@ public class AccountBillDataTest {
 
 	/**
 	 * Test method for {@link data.accountbilldata.AccountBillData#getExpenseID()}.
-	 * @throws RemoteException 
+	 * @throws RemoteException
 	 */
 	@Test
 	public void testGetExpenseID() throws RemoteException {
 		accountBill = new AccountBillData();
-		System.out.println(accountBill.getExpenseID());
+//		System.out.println(accountBill.getExpenseID());
 	}
 
 	/**
 	 * Test method for {@link data.accountbilldata.AccountBillData#getPayID()}.
-	 * @throws RemoteException 
+	 * @throws RemoteException
 	 */
 	@Test
 	public void testGetPayID() throws RemoteException {
 		accountBill = new AccountBillData();
-		System.out.println(accountBill.getPayID());
+//		System.out.println(accountBill.getPayID());
+	}
+
+	/**
+	 * Test method for {@link data.CommonData#show()}.
+	 * @throws RemoteException 
+	 */
+	@Test
+	public void testShow() throws RemoteException {
+		ArrayList<AccountBillPO> bills = new AccountBillData().show();
+		System.out.println(bills.size());
+		for(AccountBillPO po : bills) {
+			System.out.println(po.getID());
+			System.out.println(po.getState());
+			System.out.println();
+		}
 	}
 
 }
