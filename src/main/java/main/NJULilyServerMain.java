bdcc1b0f1cdb8dfc62fe6bebc51dbfc2caeb0245
cylong1016 <cyl19941016@sina.com>
2014-12-19 00:48:36 +0800

    修改了Main类的名字，在导出的时候容易找到main函数的类

--- src/main/java/main/NJULilyServerMain.java
+++ src/main/java/main/NJULilyServerMain.java
@@ -6,7 +6,7 @@ import ui.frame.ServerFrame;
  * @author cylong
  * @version Nov 2, 2014 3:17:43 PM
  */
-public class Main {
+public class NJULilyServerMain {
 
 	public static void main(String[] args) {
 		System.setProperty("awt.useSystemAAFontSettings", "on");
